bcf8d77593a27b2007092511a43b80471981e038
SofiaKotseva <Sofia.Kotseva@sky.uk>
2023-08-25 11:36:59 +0100

    message

---
+++ JS/05_JavaScriptObjects/starter/main.js
@@ -0,0 +1,46 @@
+let darthVader = {
+    allegiance : "Empire",
+    weapon : "lightsabre",
+    sith: true
+}
+
+console.log(darthVader.allegiance)
+
+console.log(darthVader.weapon)
+
+console.log(darthVader.sith)
+
+console.log(darthVader.Jedi)
+
+console.log(Object.keys(darthVader).length);
+
+darthVader.children = 2
+
+darthVader.childNames = ["Luke", "Leia"]
+
+console.log(darthVader.children)
+
+console.log(darthVader.childNames[0])
+
+for (const key in darthVader) {
+    console.log("key: ", key, "value: ", darthVader[key])   
+    }
+
+darthVader.allegiance = "The light side"
+
+console.log (darthVader)
+
+delete darthVader.children
+
+console.log (darthVader)
+
+let{allegiance, weapon, sith, childNames} = darthVader;
+
+console.log (allegiance)
+console.log (weapon)
+console.log (sith)
+console.log (childNames)
+
+darthVader = {}
+
+console.log(darthVader)
